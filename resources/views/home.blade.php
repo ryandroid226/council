f1bfbda12f701789b203e89e967fcc8cf40c0887
ryandroid226 <rlholt0000@gmail.com>
2020-10-25 11:31:22 -0400

    Removed test comment from home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,6 @@
 @section('content')
 <div class="container">
     <div class="row">
-      <!-- Adding Comment here for testing -->
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
